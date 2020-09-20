64500117bc7c91639eaff7581c34336c6b0635d8
Josh Krauth-Harding <josh@rectangle.design>
2020-09-20 14:16:39 -0400

    All content in

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,5 @@
+// RESIZE COLUMNS
+
 let currentHandle = null;
 let mouseStartX = null;
 let mouseX = null;
@@ -22,17 +24,36 @@ document.onmousemove = event => mouseX = event.clientX;
 
 function update() {
   if(currentHandle !== null) {
-	
+
     // we are holding a handle, resize the columns
     let handleIndex = [].indexOf.call(handles, currentHandle);
     let expandingColumn = columns[handleIndex];
     let shrinkingColumn = columns[handleIndex + 1];
-	
+
     expandingColumn.style = `width: calc(20% + ${mouseX - mouseStartX }px)`;
 	shrinkingColumn.style = `width: calc(20% - ${mouseX - mouseStartX}px)`;
-	
-	console.log(shrinkingColumn.lastWidth);
+
   }
   requestAnimationFrame(update); // calls update() once per frame (preferred over setInterval for browser optimisation when in background)
 }
 update();
+
+// IMG ON HOVER
+document.querySelectorAll('.example').forEach(example => {
+  const imageElement = example.querySelector('.image-element');
+
+  example.querySelector('p').addEventListener('mouseover', e => {
+    imageElement.classList.add('visible');
+  });
+
+  // example.querySelector('p').addEventListener('mousemove', follow);
+  //
+  // function follow() {
+  //   var el = example.querySelector('.image-element');
+  //   el.style.top = (event.clientY + window.scrollY + 20) + "px";
+  //   el.style.left = (event.clientX + window.scrollX - 150) + "px";
+  // }
+  example.querySelector('p').addEventListener('mouseout', e => {
+    imageElement.classList.remove('visible');
+  });
+});
